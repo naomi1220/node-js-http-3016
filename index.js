466acf01cb95de5adb8c419bd9bade9a71b09279
naomi1220 <can7034@gmail.com>
2022-11-09 22:01:58 +0900

    pathを修正

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const server = http
         if (req.url === '/') {
           res.write('<!DOCTYPE html><html lang="ja"><body>' +
             '<h1>アンケートフォーム</h1>' +
-            '<a href="http://soroban.nnn.ed.nico/enquetes">アンケート一覧</a>' +
+            '<a href="/enquetes">アンケート一覧</a>' +
             '</body></html>');
         } else if (req.url === '/enquetes') {
           res.write('<!DOCTYPE html><html lang="ja"><body>' +
             '<h1>アンケート一覧</h1><ul>' +
-            '<li><a href="http://soroban.nnn.ed.nico/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
-            '<li><a href="http://soroban.nnn.ed.nico/enquetes/rice-bread">ごはん・パン</a></li>' +
-            '<li><a href="http://soroban.nnn.ed.nico/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+            '<li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
+            '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+            '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
             '</ul></body></html>');
         } else if (req.url === '/enquetes/yaki-shabu') {
           res.write(
